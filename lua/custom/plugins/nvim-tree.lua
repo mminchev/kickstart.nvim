da8614a8a3cbb49ae6402918a8a4da0f32eb7ad4
Martin Minchev <minchev.martin@protonmail.com>
2026-02-20 00:23:04 +0200

    switched to neo-tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,17 +1,17 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
-  lazy = false,
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>tf', ':NvimTreeFindFileToggle<CR>', { desc = '[T]oggle [F]ile tree' })
-    vim.keymap.set('n', '<leader>ff', ':NvimTreeFindFile<CR>', { desc = '[F]ile tree [F]ind' })
-    require('nvim-tree').setup {
-      view = {
-        width = 40,
-      },
-    }
-  end,
+  --   'nvim-tree/nvim-tree.lua',
+  --   version = '*',
+  --   lazy = false,
+  --   dependencies = {
+  --     'nvim-tree/nvim-web-devicons',
+  --   },
+  --   config = function()
+  --     vim.keymap.set('n', '<leader>tf', ':NvimTreeFindFileToggle<CR>', { desc = '[T]oggle [F]ile tree' })
+  --     vim.keymap.set('n', '<leader>ff', ':NvimTreeFindFile<CR>', { desc = '[F]ile tree [F]ind' })
+  --     require('nvim-tree').setup {
+  --       view = {
+  --         width = 40,
+  --       },
+  --     }
+  --   end,
 }
